c36c81fe319041ed7de2bbe7aa61be166560288c
Leander <leander.vdheuvel@gmail.com>
2021-12-08 22:33:55 +0100

    added ponder class
    wat er staat

---
+++ src/Agents/Ponder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by leand on 8/12/2021.
+//
+
+#include "Ponder.h"
---
+++ src/Agents/Ponder.h
@@ -0,0 +1,14 @@
+//
+// Created by leand on 8/12/2021.
+//
+
+#ifndef INC_5_AI_CHESS_PONDER_H
+#define INC_5_AI_CHESS_PONDER_H
+
+
+class Ponder {
+
+};
+
+
+#endif //INC_5_AI_CHESS_PONDER_H
