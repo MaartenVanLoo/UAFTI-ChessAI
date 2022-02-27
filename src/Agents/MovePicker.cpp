299fda6eebf6fb3f8ef95a694788c19b838f8b2f
Maarten Van Loo <maarten.Vanloo1998@gmail.com>
2022-02-27 17:54:51 +0100

    added movepicker with killer moves

---
+++ src/Agents/MovePicker.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Maarten Van Loo on 27/02/2022.
+//
+
+#include "MovePicker.h"
---
+++ src/Agents/MovePicker.h
@@ -0,0 +1,16 @@
+//
+// Created by Maarten Van Loo on 27/02/2022.
+//
+
+#ifndef CMAKELISTS_TXT_MOVEPICKER_H
+#define CMAKELISTS_TXT_MOVEPICKER_H
+
+
+
+class MovePicker {
+
+};
+
+
+
+#endif //CMAKELISTS_TXT_MOVEPICKER_H
